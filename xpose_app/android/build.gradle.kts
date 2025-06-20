8548e80c0c36b0aa8026b3f76201921c43d1debc
nibin-joseph05 <nibin.joseph.career@gmail.com>
2025-06-20 22:12:19 +0530

    firebase added havent configured

--- xpose_app/android/build.gradle.kts
+++ xpose_app/android/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -11,17 +21,9 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15")
-    }
 }
