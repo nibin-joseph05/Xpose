291ea2f74e352d2ba508fbbd8b2a6b9cc70bb64e
nibin-joseph05 <nibin.joseph.career@gmail.com>
2025-06-20 19:58:10 +0530

    added spring boot backend

---
+++ xpose-backend/src/main/java/com/crimereport/xpose/XposeApplication.java
@@ -0,0 +1,13 @@
+package com.crimereport.xpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XposeApplication.class, args);
+	}
+
+}
---
+++ xpose-backend/src/test/java/com/crimereport/xpose/XposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimereport.xpose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
