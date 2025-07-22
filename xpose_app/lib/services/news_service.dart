df8645e6dc655eed13e43c31fcc6646926feabe6
nibin-joseph05 <nibin.joseph.career@gmail.com>
2025-07-22 20:12:23 +0530

    the news section is updated crime news in india:2

--- xpose_app/lib/services/news_service.dart
+++ xpose_app/lib/services/news_service.dart
@@ -6,7 +6,7 @@ class NewsService {
   static Future<List<dynamic>> getIndiaTopHeadlines() async {
     final baseUrl = dotenv.env['API_BASE_URL'];
     final response = await http.get(
-      Uri.parse('$baseUrl/api/news/india-crime-news'),
+      Uri.parse('$baseUrl/api/news/india-top-headlines'),
       headers: {'Content-Type': 'application/json'},
     );
 
